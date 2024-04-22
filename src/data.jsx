d54867ef94ef7729f3c68b3cd0a794f9c1e28305
Ashil ihsan <ashilihsan@gmail.com>
2024-04-22 20:31:28 +0530

    first commit-all files to repository

---
+++ src/data.jsx
@@ -0,0 +1,34 @@
+export const projects = [
+    {
+      title: "React Reserve",
+      subtitle: "MERN Stack",
+     image: "./project-1.gif",
+      link: "https://reactbootcamp.com",
+    },
+    {
+      title: "React Tracks",
+      subtitle: "React and Python",
+       image: "./project-2.gif",
+      link: "https://reedbarger.com",
+    },
+    {
+      title: "DevChat",
+      subtitle: "React and Firebase",
+      image: "./project-3.gif",
+      link: "https://jsbootcamp.com",
+    },
+    {
+      title: "Epic Todo App",
+      subtitle: "React Hooks",
+     image: "./project-4.gif",
+      link: "https://pythonbootcamp.com",
+    },
+  ];
+  export const skills = [
+    {title:"react",
+    id:"1"},
+    {title:"JavaScript",
+  id:"2"},
+  {title:"Nodejs",
+  id:"3"}
+  ]
